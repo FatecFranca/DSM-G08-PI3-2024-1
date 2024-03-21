802314dfdba4a3c7eb39536c9ad2dfb8736d25bd
kefflen <kefflenjava@hotmail.com>
2024-03-21 20:37:16 -0300

    styles: add .editorconfig, eslint and prettier. And apply styles rules

--- backend/src/configs/env.ts
+++ backend/src/configs/env.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
-dotenv.config()
 import { z } from 'zod'
+dotenv.config()
 
 const envSchema = z.object({
   PORT: z.string().default('8080'),
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
-import { env } from "./configs/env"
-import server from "./configs/server"
+import { env } from './configs/env'
+import server from './configs/server'
 
 
 server.listen(env.PORT, () => console.log(`Server runing at: http://localhost:${env.PORT}`))
