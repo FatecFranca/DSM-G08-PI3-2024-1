2a4a05e55ed095ec8d8cb39cf7ec64118d8b3e3e
kefflen <kefflenjava@hotmail.com>
2024-03-22 09:30:47 -0300

    refactor: change error message in mongoose connect catch

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,10 @@ import mongoose from 'mongoose'
 import { env } from './configs/env'
 
 import server from './configs/server'
-
-mongoose.connect(env.DATABASE_URL)
+mongoose.connect('mongodb://localhost:27017/saudeOn')
   .then(() => {
     server.listen(env.PORT, () => console.log(`Server runing at: http://localhost:${env.PORT}`))
   })
   .catch(err => {
-    console.log('Error connecting to the database: ', err)
+    console.log(err)
   })
