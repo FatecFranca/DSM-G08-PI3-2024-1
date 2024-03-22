fb0fa79575540cb58a6eeda379a10e53c4fb41ab
kefflen <kefflenjava@hotmail.com>
2024-03-22 09:17:18 -0300

    feat: create connection to mongodb with mongoose

--- backend/src/configs/env.ts
+++ backend/src/configs/env.ts
@@ -4,6 +4,7 @@ dotenv.config()
 
 const envSchema = z.object({
   PORT: z.string().default('8080'),
+  DATABASE_URL: z.string()
 })
 
 export const env = envSchema.parse(process.env)
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,12 @@
+import mongoose from 'mongoose'
 import { env } from './configs/env'
+
 import server from './configs/server'
 
-
-server.listen(env.PORT, () => console.log(`Server runing at: http://localhost:${env.PORT}`))
+mongoose.connect(env.DATABASE_URL)
+  .then(() => {
+    server.listen(env.PORT, () => console.log(`Server runing at: http://localhost:${env.PORT}`))
+  })
+  .catch(err => {
+    console.log('Error connecting to the database: ', err)
+  })
