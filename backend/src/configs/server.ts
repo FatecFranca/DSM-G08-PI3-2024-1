6021bd9c39426b89278b032b5bb90c3dbb44a0be
kefflen <kefflenjava@hotmail.com>
2024-04-22 12:04:28 -0300

    feat: add swagger express ui

--- backend/src/configs/server.ts
+++ backend/src/configs/server.ts
@@ -2,15 +2,19 @@ import cors from 'cors'
 import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import http from 'http'
-import { ZodError } from 'zod'
-import routes from './routes'
 import { Server } from 'socket.io'
+import swaggerUi from 'swagger-ui-express'
+import { ZodError } from 'zod'
+import swaggerFile from '../swagger_output.json'
+import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use(routes)
+app.use('/api-docs', swaggerUi.serve)
+app.get('/api-docs', swaggerUi.setup(swaggerFile))
 
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof ZodError) {
