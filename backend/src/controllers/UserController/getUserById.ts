ae936f99bdac240eb3450743acc9a6a7b1b470c5
kefflen <kefflenjava@hotmail.com>
2024-04-23 20:58:59 -0300

    refactor: isolate getById tests

--- backend/src/controllers/UserController/getUserById.ts
+++ backend/src/controllers/UserController/getUserById.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from 'express'
-import { userModel } from '../../models/UserModel'
+import { AppError } from '../../errors/AppError'
 import { NotFoundError } from '../../errors/NotFoundError'
+import { userModel } from '../../models/UserModel'
+import { RoleEnum } from '../../types/RoleEnum'
 
 export const getUserById = async (req: Request, res: Response) => {
+  const payload = req.payload
+  if (!payload) {
+    throw AppError.internalServerError('Payload not found')
+  }
+  const isNotEmployee = ![RoleEnum.ADMIN, RoleEnum.EMPLOYEE].includes(payload.role)
+  if (isNotEmployee && payload.id !== req.params.id) {
+    throw AppError.forbidden('You are not allowed to access this resource')
+  }
+  
   const { id } = req.params
 
   const user = await userModel.findById(id)
