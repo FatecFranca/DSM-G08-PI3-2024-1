80c5ae1eeac21a01f3147760194f1171a198a82e
kefflen <kefflenjava@hotmail.com>
2024-05-25 10:38:09 -0300

    feat: add next auth using credentials providers

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,4 +1,5 @@
-import { Inter } from "next/font/google";
+import { Inter } from "next/font/google"
+import { Providers } from "./providers"
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Providers>
+        <body className={inter.className}>{children}</body>
+      </Providers>
     </html>
   );
 }
---
+++ frontend/src/app/providers.jsx
@@ -0,0 +1,10 @@
+'use client'
+import { SessionProvider } from 'next-auth/react'
+
+export const Providers = ({ children }) => {
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  )
+}
