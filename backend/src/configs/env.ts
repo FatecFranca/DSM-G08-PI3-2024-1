618b99327ce1884d9e672ffce141b3db010e042c
kefflen <kefflenjava@hotmail.com>
2024-06-08 11:00:57 -0300

    feat: create admin on first initialization

--- backend/src/configs/env.ts
+++ backend/src/configs/env.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
-import { z } from 'zod'
-
+import { string, z } from 'zod'
+//TODO: Create variable to create admin user with default password and email. OPTIONAL
 const environment = process.env.NODE_ENV || 'dev'
 if (environment === 'dev') {
   dotenv.config({
@@ -20,6 +20,8 @@ export const envSchema = z.object({
   DATABASE_URL: z.string(),
   ENV: ENVZodType,
   JWT_SECRET: z.string(),
+  ADMIN_EMAIL: z.string().email(),
+  ADMIN_PASSWORD: string().min(8)
 })
 
 export const env = envSchema.parse(process.env)
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,41 @@ import './configs/websockets'
 import { env } from './configs/env'
 
 import app from './configs/server'
+import { employeeModel } from './models/EmployeeModel'
+import { RoleEnum } from './types/RoleEnum'
+import { userModel } from './models/UserModel'
+
 mongoose.connect('mongodb://localhost:27017/saudeOn')
-  .then(() => {
+  .then(async () => {
+    const hasAdmin = await employeeModel.findOne({
+      role: RoleEnum.ADMIN
+    })
+
+    if (!hasAdmin) {
+      const adminUser = await userModel.create({
+        cpf: 'undefined',
+        data_nascimento: new Date(),
+        email: env.ADMIN_EMAIL,
+        password: env.ADMIN_PASSWORD,
+        gender: 'undefined',
+        lastName: 'ADMIN',
+        name: 'Admin',
+        address: {
+          cep: 'undefined',
+          street: 'undefined',
+          num: 0,
+          city: 'undefined',
+          uf: 'undefined'
+        }
+      })
+
+      await employeeModel.create({
+        role: RoleEnum.ADMIN,
+        user: adminUser._id
+      })
+    }
+
+
     app.listen(env.PORT, () => console.log(`Server runing at: http://localhost:${env.PORT}`))
   })
   .catch(err => {
