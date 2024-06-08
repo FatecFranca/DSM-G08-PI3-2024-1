0f7dc00bb4eb68154b3093c776286c4a1ddc5881
kefflen <kefflenjava@hotmail.com>
2024-06-08 18:59:16 -0300

    fix: update paciente page

--- frontend/src/app/(authenticated)/page.jsx
+++ frontend/src/app/(authenticated)/page.jsx
@@ -1,13 +1,14 @@
 'use client'
+import { useUserSession } from "../hooks/useUserSession"
 import Dashboard from "./components/dashboard/Dashboard"
-import Sidebar from "./components/sidebar/Sidebar"
+import Paciente from "./components/paciente/Paciente"
 import { Container } from "./styles"
 
 export default function Home() {
-
+  const { session: { role } } = useUserSession()
   return (
     <Container>
-      <Dashboard />
+      {role == 'user' ? (<Paciente />) : (<Dashboard />)}
     </Container>
   )
 }
