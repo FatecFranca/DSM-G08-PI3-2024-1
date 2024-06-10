46d00915b668e77720635b6043f4c8b27269a523
kefflen <kefflenjava@hotmail.com>
2024-06-10 17:53:44 -0300

    add eviromento variavel NEXT_PUBLIC_API_URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL:   process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
   },
